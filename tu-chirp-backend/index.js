b52b015a49995e56394201e5b62c73d2de4d2327
Katie Winner <katelyn.blomquist@gmail.com>
2024-05-11 08:13:10 -0400

    update queries

--- tu-chirp-backend/index.js
+++ tu-chirp-backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { getAllPosts } = require('./db');
+const { getAllPosts, getAllPostsHandles, getPostById, updatePost, deletePost, favoritePost, getPostByUser, createComment, getUserInfo } = require('./db');
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000
