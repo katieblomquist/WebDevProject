9e7d9034da84055b7127e6ce0dc0d00e403a4e21
Katie Winner <katelyn.blomquist@gmail.com>
2024-05-11 12:13:46 -0400

    finish get post by user query

--- tu-chirp-backend/db.js
+++ tu-chirp-backend/db.js
@@ -49,7 +49,7 @@ async function favoritePost(user_id, content_id){
 }
 
 async function getPostByUser(user_id){
-  const result = await sql `SELECT post.*, users.handle FROM post INNER JOIN users ON post.user_id = users.id where user_id = ${user_id} AND is_deleted = false`;
+  const result = await sql `SELECT post.*, users.handle FROM post INNER JOIN users ON post.user_id = users.id WHERE user_id = ${user_id} AND is_deleted = false AND post.is_post = true`;
   return result;
 }
 
