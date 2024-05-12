591ab33b3dd0160478b70054e9a242bd91defbcd
hazoo2a <hazoo2a@example.com>
2024-05-12 19:54:34 -0400

    account and post

--- tu-chirp-frontend/src/routes/account.tsx
+++ tu-chirp-frontend/src/routes/account.tsx
@@ -1,11 +1,19 @@
-export default function Account(){
-    // implement hooks
+import React from 'react';
+import '/src/index.css';  //this is the only css file that have styling on it, the app.css file doesn't have any thing
 
-    //implement functions
-
-    return(
-        <>
-            <h1>The Account Page</h1>
-        </>
+function AccountPage() {
+    return (
+        <div className="account-container">
+            <div className="profile-card">
+                <div className="profile-header">
+                    <h1>Katelyn Winner</h1>
+                    <p>@princess.katelyn</p>
+                    <p>katelyn.winner@gmail.com</p>
+                </div>
+                <button className="delete-profile">Delete Profile</button>
+            </div>
+        </div>
     );
 }
+
+export default AccountPage;
